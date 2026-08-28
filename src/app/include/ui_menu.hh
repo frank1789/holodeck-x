504e2ef5cf4a04ee9891d8b416163c662d3a4888
Francesco Argentieri <francesco.argentieri89@gmail.com>
2026-08-28 23:45:00 +0100

    feat: add basic menu

---
+++ src/app/include/ui_menu.hh
@@ -0,0 +1,23 @@
+#ifndef UI_MENU_HH_
+#define UI_MENU_HH_
+
+#include <string_view>
+#include <functional>
+#include <span>
+
+struct MenuItem {
+    std::string_view label;
+    std::string_view shortcut;
+    std::function<int()> callback;
+    std::function<bool()> checked;
+    std::function<bool()> enabled;
+    std::uint64_t id;
+};
+
+struct MenuNode {
+    std::string_view label;
+    std::span<const MenuItem> items;
+    std::span<const MenuNode> children;
+}
+
+#endif // UI_MENU_HH_
